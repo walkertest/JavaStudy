abea5afe02a3337315dc7be4ffd19175db25b14a
walker <zhaoyu@huya.com>
2022-02-17 14:17:56 +0800

    add test

--- src/main/java/com/tencent/concurrent/Collection/CompleteFutureTest.java
+++ src/main/java/com/tencent/concurrent/Collection/CompleteFutureTest.java
@@ -1,7 +1,40 @@
 package com.tencent.concurrent.Collection;
 
+import java.util.concurrent.*;
+
 /**
  * Created by walker on 2019/5/26.
  */
 public class CompleteFutureTest {
+
+    public static ThreadPoolExecutor bizPoolExecutor = new ThreadPoolExecutor(
+            4,
+            8,
+            0L,
+            TimeUnit.SECONDS,
+            new LinkedBlockingDeque<>(10000),
+            Executors.defaultThreadFactory(),
+            new ThreadPoolExecutor.CallerRunsPolicy());
+
+
+    public static void main(String[] args) throws InterruptedException {
+        CompletableFuture<String> data = CompletableFuture.completedFuture("suc");
+        System.out.println(System.getProperty("java.util.concurrent.ForkJoinPool.common.parallelism"));
+        System.out.println(ForkJoinPool.getCommonPoolParallelism());
+
+        testForkJoin();
+
+        data.handleAsync((result,error) ->{
+            System.out.println(System.getProperty("java.version"));
+            System.out.println("threadName:" + Thread.currentThread().getName());
+            System.out.println(result);
+            return null;
+        }, bizPoolExecutor);
+        Thread.sleep(5000);
+    }
+
+    private static void testForkJoin() {
+        int size = ForkJoinPool.commonPool().getPoolSize();
+        System.out.println(size);
+    }
 }
